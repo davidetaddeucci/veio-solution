22824461561c0edecf43de269b9027a571400d5e
davidetaddeucci <info@hybrid.it>
2025-02-16 11:34:19 +0100

    Add files via upload

---
+++ Hybrid.Veio.Names/Models/Models.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hybrid.Veio.Names.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = "User";
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public class LoginRequest
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+
+    public class AuthResponse
+    {
+        public string Token { get; set; } = null!;
+        public DateTime Expiration { get; set; }
+    }
+    public class UserRegistration
+    {
+        [Required]
+        public string Email { get; set; } = null!; // Utilizzato anche come username
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        public string Role { get; set; } = "User"; // Ruolo predefinito
+    }
+}
